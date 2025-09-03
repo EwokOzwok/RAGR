ed7e5d67bb54165145183675892941ce98e2ed27
Ozmat <eozmat@albany.edu>
2025-09-03 11:30:17 -0400

    update

--- dev/01_start.R
+++ dev/01_start.R
@@ -40,7 +40,7 @@ golem::install_dev_deps()
 ## See ?usethis for more information
 # usethis::use_mit_license("Golem User") # You can set another license here
 usethis::use_apache_license()
-golem::use_readme_rmd(open = FALSE)
+golem::use_readme_rmd(open = T)
 devtools::build_readme()
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
